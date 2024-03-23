d50fd828742914e5fc33abbb6ed96b642a0f2dde
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-03-23 21:30:46 +0530

    vercel added

--- frontend/src/components/OtherUsersProfilePage.tsx
+++ frontend/src/components/OtherUsersProfilePage.tsx
@@ -1,5 +1,5 @@
 // pages/UserProfilePage.js
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
