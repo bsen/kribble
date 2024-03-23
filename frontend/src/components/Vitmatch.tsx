2935fd8329c851bdc0737f4712ca29f5bf18ad87
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-03-24 03:12:05 +0530

    storage user logic updated

--- frontend/src/components/Vitmatch.tsx
+++ frontend/src/components/Vitmatch.tsx
@@ -3,13 +3,13 @@ import CloseIcon from "@mui/icons-material/Close";
 
 export const Vitmatch = () => {
   return (
-    <div className="h-screen w-[50%] bg-black ">
+    <div className="h-screen w-[50%] bg-black flex flex-col justify-between">
       <div className="w-full flex justify-center">
         <div className="text-3xl bg-black  text-white font-mono w-[80%] text-center py-5 border-b border-bordercolor">
           Vit Match
         </div>
       </div>
-      <div className="w-full flex flex-col items-center justify-center h-[50vw]">
+      <div className="w-full flex flex-col items-center justify-center ">
         <img
           src="https://imagedelivery.net/cV-2jw5Z4EJcAnIlwLPzWw/5da2ca8e-0167-4468-a160-9a8a541c2700/public"
           className="h-[50vh] rounded-xl"
@@ -34,6 +34,9 @@ export const Vitmatch = () => {
           </div>
         </div>
       </div>
+      <p className="my-4 text-center font-mono font-thin text-xs text-bordercolor">
+        © 2024 undate Ltd / biswarupz production
+      </p>
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        bordercolor: "rgba(255, 255, 255, 0.30)",
+        bordercolor: "rgba(255, 255, 255, 0.20)",
         logos: "rgba(255, 255, 255, 0.6)",
         background: "rgba(18, 21, 23, 1,0.5)",
       },
