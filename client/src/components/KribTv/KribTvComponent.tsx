f4e05a8f5fffc8af27360b42a7313b3863f4c670
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-04-27 19:16:49 +0530

    video calling

--- client/src/components/KribTv/KribTvComponent.tsx
+++
@@ -1,7 +0,0 @@
-export const KribTvConnect = () => {
-  return (
-    <div className="text-[3rem] h-screen font-ubuntu flex justify-center items-center">
-      <div>coming soon</div>
-    </div>
-  );
-};
