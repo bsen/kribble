eaa4079e5215cbf4060bfc6267d12ce3ffe8a250
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-04-30 22:15:29 +0530

    community all

--- server/src/routes/community.ts
+++ server/src/routes/community.ts
@@ -89,3 +89,46 @@ communityRouter.post("/create", async (c) => {
     console.log(error);
   }
 });
+
+communityRouter.post("/all/communities", async (c) => {
+  try {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+    const token = body.token;
+    const userId = await verify(token, c.env.JWT_SECRET);
+    const findUser = await prisma.user.findUnique({ where: { id: userId.id } });
+    if (!findUser) {
+      return c.json({ status: 401, message: "Unauthorized" });
+    }
+    const cursor = body.cursor || null;
+    const take = 15;
+    const allCommunities = await prisma.community.findMany({
+      select: {
+        id: true,
+        image: true,
+        name: true,
+        category: true,
+        description: true,
+        memberCount: true,
+      },
+      orderBy: {
+        memberCount: "asc",
+      },
+      cursor: cursor ? { id: cursor } : undefined,
+      take: take + 1,
+    });
+
+    const hasMore = allCommunities.length > take;
+    const communities = hasMore ? allCommunities.slice(0, -1) : allCommunities;
+    const nextCursor = hasMore
+      ? allCommunities[allCommunities.length - 1].id
+      : null;
+
+    return c.json({ status: 200, data: communities, nextCursor });
+  } catch (error) {
+    console.log(error);
+    return c.json({ status: 400 });
+  }
+});
