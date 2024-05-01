b6959be5a863f39c7a5f29c2f092a2898c7e8bd0
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-05-02 02:42:39 +0530

    pagination for phone

--- client/src/components/ProfileComponents/CommentsComponent.tsx
+++ client/src/components/ProfileComponents/CommentsComponent.tsx
@@ -92,85 +92,87 @@ export const CommentsComponent = () => {
   };
   return (
     <>
-      {deleteState ? (
-        <div className="w-full h-screen flex justify-center items-center">
-          <div className="flex flex-col gap-4 text-base  items-center font-ubuntu font-semibold">
-            Do you really want to delete the post
-            <span className="text-xs font-light text-neutral-600">
-              note you can not get back the deleted item!
-            </span>
-            <div className="flex gap-5">
-              <button
-                onClick={deleteComment}
-                className="text-white bg-red-500 hover:bg-red-400 font-semibold px-4 py-1  rounded-full"
-              >
-                Delete
-              </button>
-              <button
-                onClick={() => {
-                  setDeleteState(false);
-                  setDeleteCommentId("");
-                  setDeleteCommentPostId("");
-                }}
-                className="text-black bg-background hover:bg-neutral-200 font-semibold px-4 py-1 border border-neutral-300 rounded-full"
-              >
-                Cancel
-              </button>
+      <div className="h-screen">
+        {deleteState ? (
+          <div className="w-full h-screen flex justify-center items-center">
+            <div className="flex flex-col gap-4 text-base  items-center font-ubuntu font-semibold">
+              Do you really want to delete the post
+              <span className="text-xs font-light text-neutral-600">
+                note you can not get back the deleted item!
+              </span>
+              <div className="flex gap-5">
+                <button
+                  onClick={deleteComment}
+                  className="text-white bg-red-500 hover:bg-red-400 font-semibold px-4 py-1  rounded-full"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => {
+                    setDeleteState(false);
+                    setDeleteCommentId("");
+                    setDeleteCommentPostId("");
+                  }}
+                  className="text-black bg-background hover:bg-neutral-200 font-semibold px-4 py-1 border border-neutral-300 rounded-full"
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <div
-          className="h-screen overflow-y-auto no-scrollbar py-14"
-          onScroll={handleScroll}
-          ref={commentsScrollContainerRef}
-        >
-          <SearchBox />
-          {commentsData.comments.length > 0 ? (
-            commentsData.comments.map((comment) => (
-              <div
-                key={comment.id}
-                className="border-b border-neutral-200 p-4 hover:bg-neutral-50"
-              >
-                <div className="flex flex-col gap-2 ">
-                  <div className="text-primarytextcolor w-max flex items-center justify-between gap-2 text-sm font-light">
-                    <Link to={`/post/${comment.postId}`}>
-                      <OpenInNewIcon
-                        sx={{ fontSize: 20 }}
-                        className="text-blue-500"
-                      />
-                    </Link>
-                    {comment.createdAt.slice(0, 10)}{" "}
-                    <div className="text-neutral-600">
-                      <button
-                        onClick={() => {
-                          setDeleteCommentId(comment.id);
-                          setDeleteCommentPostId(comment.postId);
-                          setDeleteState(true);
-                        }}
-                      >
-                        <MoreVertIcon sx={{ fontSize: 18 }} />
-                      </button>
+        ) : (
+          <div
+            className="h-screen overflow-y-auto no-scrollbar py-14"
+            onScroll={handleScroll}
+            ref={commentsScrollContainerRef}
+          >
+            <SearchBox />
+            {commentsData.comments.length > 0 ? (
+              commentsData.comments.map((comment) => (
+                <div
+                  key={comment.id}
+                  className="border-b border-neutral-200 p-4 hover:bg-neutral-50"
+                >
+                  <div className="flex flex-col gap-2 ">
+                    <div className="text-primarytextcolor w-max flex items-center justify-between gap-2 text-sm font-light">
+                      <Link to={`/post/${comment.postId}`}>
+                        <OpenInNewIcon
+                          sx={{ fontSize: 20 }}
+                          className="text-blue-500"
+                        />
+                      </Link>
+                      {comment.createdAt.slice(0, 10)}{" "}
+                      <div className="text-neutral-600">
+                        <button
+                          onClick={() => {
+                            setDeleteCommentId(comment.id);
+                            setDeleteCommentPostId(comment.postId);
+                            setDeleteState(true);
+                          }}
+                        >
+                          <MoreVertIcon sx={{ fontSize: 18 }} />
+                        </button>
+                      </div>
+                    </div>
+                    <div className="text-primarytextcolor  text-sm lg:text-base font-light">
+                      {comment.content}
                     </div>
                   </div>
-                  <div className="text-primarytextcolor  text-sm lg:text-base font-light">
-                    {comment.content}
-                  </div>
                 </div>
+              ))
+            ) : (
+              <div className="text-center font-ubuntu my-5 text-primarytextcolor">
+                No Comments found.
               </div>
-            ))
-          ) : (
-            <div className="text-center font-ubuntu my-5 text-primarytextcolor">
-              No Comments found.
-            </div>
-          )}
-          {isLoadingComments && (
-            <div className="text-center my-5 text-gray-500">
-              <CircularProgress />
-            </div>
-          )}
-        </div>
-      )}
+            )}
+            {isLoadingComments && (
+              <div className="text-center my-5 text-gray-500">
+                <CircularProgress />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
     </>
   );
 };
