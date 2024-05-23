55c65c2d4084fe7fd38d3b0b58ab943bb012e027
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-05-23 17:38:43 +0530

    bottom bar adn incognito

--- client/src/components/User/IncognitoPosts/IncognitoPostsComponent.tsx
+++ client/src/components/User/IncognitoPosts/IncognitoPostsComponent.tsx
@@ -333,15 +333,16 @@ export const IncognitoPostsComponent: React.FC<ProfileSectionProps> = () => {
               </div>
             ))
           ) : (
-            <div className="text-texttwo my-5  font-light text-center text-sm">
-              No posts found
-            </div>
-          )}
-        </div>
-        <div>
-          {isLoading && (
-            <div className="w-full my-5 flex justify-center items-center">
-              <CircularProgress />
+            <div>
+              {isLoading ? (
+                <div className="w-full my-5 flex justify-center items-center">
+                  <CircularProgress sx={{ color: "rgb(50 50 50);" }} />
+                </div>
+              ) : (
+                <div className="text-texttwo my-5 font-light text-center text-lg">
+                  No posts found
+                </div>
+              )}
             </div>
           )}
         </div>
