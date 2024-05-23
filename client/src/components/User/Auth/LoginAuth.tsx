950522d25c28095cac8899d4243946e87a03decb
biswarupz <126864631+biswarupz@users.noreply.github.com>
2024-05-23 22:22:13 +0530

    signup and login ui

--- client/src/components/User/Auth/LoginAuth.tsx
+++ client/src/components/User/Auth/LoginAuth.tsx
@@ -12,10 +12,12 @@ export const LoginAuth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [popup, setPopup] = useState("");
-  const handleEmailChnage = (text: string) => {
+
+  const handleEmailChange = (text: string) => {
     const newEmail = text.toLowerCase();
     setEmail(newEmail);
   };
+
   async function login() {
     if (!email || !password) {
       setPopup("Please fill in all the fields");
@@ -29,7 +31,7 @@ export const LoginAuth = () => {
         data
       );
 
-      if (response.data.status == 200) {
+      if (response.data.status === 200) {
         localStorage.setItem("token", response.data.token);
         setCurrentUser(response.data.username);
         navigate("/");
@@ -40,17 +42,21 @@ export const LoginAuth = () => {
       setIsLoading(false);
     } catch (error) {
       console.log(error);
+      setPopup("An error occurred. Please try again.");
+      setIsLoading(false);
     }
   }
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter" && !event.shiftKey) {
       event.preventDefault();
       login();
     }
   };
+
   if (isLoading) {
     return (
-      <div className=" flex justify-center items-center h-screen w-full text-center">
+      <div className="flex justify-center items-center h-screen w-full text-center">
         <CircularProgress />
       </div>
     );
@@ -58,44 +64,66 @@ export const LoginAuth = () => {
 
   return (
     <>
-      <div className="h-screen w-full p-2  flex flex-col justify-center items-center">
-        <div className="w-[100%] lg:w-[35%]">
-          <div className="text-texttwo text-center mb-6 font-ubuntu font-medium text-3xl">
-            Welcome back to FriendCity
-          </div>
-
-          <div className="items-center p-2 rounded-lg bg-bgmain">
-            <div className="my-2 justify-center flex gap-2">
+      <div className="lg:flex justify-between items-center">
+        <div className="h-screen p-3 flex justify-center items-center w-full bg-indigomain">
+          <div className="text-texttwo text-center mb-4 font-ubuntu font-medium text-[2.5rem]">
+            FriendCity
+            <div className="text-center text-sm font-thin mb-8 text-textmain">
+              A place where college life meets limitless fun! 🎉 Share your
+              moments, snap cool pics, and dive into vibrant communities. With
+              our anonymous posting, let your thoughts soar freely. But wait,
+              there's more! Join forces with a fellow student, conquer exciting
+              challenges, and skyrocket to the top of the leaderboard! 🚀 Get
+              ready to unleash the epicness, starting at the top 20 colleges.
+              Don't miss out on the adventure – join us now!
+            </div>
+            <div className="flex flex-wrap mb-5 justify-center gap-2">
               <img
                 src="/girl.png"
-                className="h-[15%] w-[15%]  rounded-lg  bg-bgtwo"
+                className="h-[15%] w-[15%] rounded-full bg-bgtwo"
+                alt="Girl"
               />
               <img
                 src="/boy.png"
-                className="h-[15%] w-[15%]  rounded-lg  bg-bgtwo"
+                className="h-[15%] w-[15%] rounded-full bg-bgtwo"
+                alt="Boy"
               />
               <img
                 src="/people.png"
-                className="h-[15%] w-[15%]  rounded-full  bg-bgtwo"
+                className="h-[15%] w-[15%] rounded-full bg-bgtwo"
+                alt="People"
               />
-
               <img
                 src="/girl2.png"
-                className="h-[15%] w-[15%]  rounded-lg  bg-bgtwo"
+                className="h-[15%] w-[15%] rounded-full bg-bgtwo"
+                alt="Girl 2"
               />
               <img
                 src="/boy2.png"
-                className="h-[15%] w-[15%]  rounded-lg  bg-bgtwo"
+                className="h-[15%] w-[15%] rounded-full bg-bgtwo"
+                alt="Boy 2"
               />
             </div>
-
+            <button
+              onClick={() => {
+                window.scrollTo({
+                  top: window.innerHeight,
+                  behavior: "smooth",
+                });
+              }}
+              className="lg:hidden rounded-full bg-white text-indigomain px-4 py-1 text-lg"
+            >
+              Join now
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col h-screen bg-bgmain justify-center items-center p-3 w-full">
+          <div className="items-center p-2 rounded-lg bg-bgtwo w-[80%] lg:w-[50%]">
             <div>
               <div className="font-normal m-1 text-texttwo">Email</div>
               <input
                 value={email}
-                onChange={(e) => {
-                  handleEmailChnage(e.target.value);
-                }}
+                onChange={(e) => handleEmailChange(e.target.value)}
                 type="email"
                 className="h-9 w-full text-textmain rounded-lg px-4 focus:outline-none bg-bordermain"
                 placeholder="Enter your email address"
@@ -106,9 +134,7 @@ export const LoginAuth = () => {
               <div className="font-normal m-1 text-texttwo">Password</div>
               <input
                 value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
+                onChange={(e) => setPassword(e.target.value)}
                 type="password"
                 onKeyDown={handleKeyDown}
                 className="h-9 w-full text-textmain rounded-lg px-4 focus:outline-none bg-bordermain"
@@ -117,7 +143,7 @@ export const LoginAuth = () => {
             </div>
             <button
               onClick={login}
-              className="my-4 w-full text-texttwo bg-indigomain active:bg-bgmain border border-bordermain focus:outline-none focus:ring-2 focus:ring-neutral-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+              className="my-4 w-full text-texttwo bg-indigomain active:bg-bgmain border border-bordermain focus:outline-none focus:ring-2 focus:ring-neutral-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
             >
               Login
             </button>
@@ -130,17 +156,17 @@ export const LoginAuth = () => {
                 Sign up
               </Link>
             </div>
-            <div className="text-rosemain font-ubuntu font-light text-center text-sm">
-              {popup ? popup : <div>‎</div>}
-            </div>
           </div>
+          <div className="text-rosemain mt-2 font-light text-center text-xs">
+            {popup ? popup : <div>‎</div>}
+          </div>
+          <footer className="w-full font-ubuntu py-2 text-xs flex flex-col gap-2 items-center justify-center text-neutral-600">
+            © 2024 FriendCity Ltd.
+            <Link to="/policies" className="underline">
+              Policies
+            </Link>
+          </footer>
         </div>
-        <footer className="w-full font-ubuntu py-2 text-xs flex flex-col gap-2 items-center justify-center text-neutral-600">
-          © 2024 FriendCity Ltd.
-          <Link to="/policies" className="underline">
-            Policies
-          </Link>
-        </footer>
       </div>
     </>
   );
